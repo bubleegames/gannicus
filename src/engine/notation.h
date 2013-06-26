b1ea24bf4667982f2ed139450e95d482f1f399e4
H Forrest Alexander <h.forrest.alexander@gmail.com>
2013-06-26 15:39:59 -0700

    non-interfering prototypes. More of them

--- src/engine/notation.h
+++ src/engine/notation.h
@@ -1,11 +1,13 @@
 #include<array>
 #include<bitset>
+#include<vector>
 
 #ifndef ___notation
 #define ___notation
 
 using std::array;
 using std::bitset;
+using std::vector;
 
 enum diatonic {Tonic, Supertonic, Mediant, Subdominant, 
                Dominant, Submediant, Leading, DiatonicDegrees};
@@ -17,6 +19,13 @@ enum accidental {DoubleFlat=-2, Flat, Neutral, Sharp, DoubleSharp};
 
 typedef array <chromatic, DiatonicDegrees> scale;
 
+struct key
+{
+    diatonic m; //Mode/starting point in the scale
+    scale s; //Scale basis
+    chromatic f; //Fundamental
+};
+
 struct note
 {
 	accidental a;
@@ -26,24 +35,47 @@ struct note
 	size_t duration; //In frames
 };
 
-template <size_t L> class phrase : array<note, L> {};
+class phrase : vector<note> {};
 //methods: mode (output in different mode), modulate (output with root transposed), key (output in different Key), scale, etc
-template <size_t S, size_t L> class rhythm : array<bitset<S>, L> {};
-//static_assert: number of 1s = index of bitset in the array
 
-template <size_t S, size_t L> class expression : phrase<S> {};
-expression(phrase<L>, rhythm<S, L>
+template <size_t FrameLength>
+class rhythm : vector<array<size_t, FrameLength>> {};
+//static_assert: number of 1s = index of bitset in the array
+//Made this an array of size_t instead of bits, because I forgot about duration being a thing that rhythms need to handle.
+//Convenience function: diatonic transposition. Move this to another mode without modulating.
+
+template <size_t FrameLength>
+class expression : phrase //Should have FrameLength events.
+{expression(phrase, rhythm<FrameLength>, key);};
 //use rhythm of size_t S
     //construct by passing in a rhythm and a phrase.
     //get a phrase that is in the right number of frames per measure, etc
+    //apply a key. This structure should be able to output whatever is needed for a Voice to exactly play notes. So it has a Play method or whatever that's used to do that.
+    //Other convenience methods should include modulation (changing the key), etc.
+//The game should always play whole expressions, so that nothing is too janky
+//This also means that various matchups will have specific FrameLength for all phrases/recurring rhythms, etc.
 
-class key {}; //is this useful?
+template <size_t ExpressionFrameLength>
+class pattern : vector<expression<ExpressionFrameLength>>
+{
+    //A pattern is a sequence of phrases.
+    //A pattern should contain 0-n expressions. 
+    //It should have a method to select and play an expression. 
+    //This method should select what to play on the basis of game heuristics.
+    //Selectable methods should be able to:
+    //  a) maintain internal position and play vector members in a loop
+    //  b) jump to specific members if appropriate, including the one that just played
+    //  c) point to another pattern and access it instead.
+    //Patterns could be used for:
+    //  a) rhythms - e.g. there are 4 different drum tracks and 4 different fills, and one is just selected to loop back to itself until some transition, when a fill is selected, etc.
+    //  b) continuous melodies - iterate across them until they loop, then go to something else or repeat
+    //  c) patterns of patterns. Since patterns can "play" another pattern, they can be used to step up a directed graph of patterns to play, so we can use patterns to time (e.g. "4 cycles" or "end of this vector") transitions among song elements.
+    //
+    //
+    //I think a class along these lines could be the backbone of the song structure... or, more likely, subclasses of it for specific reusable things in songs like drum track or counterpoint line or whatever
 
-//waaaay too tired to syntax but yeah, i'm thinkin that phrases get played in a Key, which is a Scale + a Mode + a Modulation. Accidentals can still occur on a note-by-note basis if necessary but this should be avoided. 
-//a character might have more than one scale. 
-//however scales probably still need to be essentially diatonic in basic structure... so that phrases can be translated among keys
-//
-//
-//
-//
+};
+
+
+
 #endif
