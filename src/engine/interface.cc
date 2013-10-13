9d35adb115ce50de5b21c319fa385a1a7aee4b91
Alex Kelly <eshink@gmail.com>
2013-10-12 23:25:39 -0700

    Ugh buggy

--- src/engine/interface.cc
+++ src/engine/interface.cc
@@ -541,7 +541,7 @@ void SaltAndBone::resolveInputs()
 		for(instance *i:things){
 			bool d = 0;
 			if(i)
-				i->getMove(currentFrame[things[i]->ID - 1].buttons, d);
+				i->getMove(currentFrame[i->ID - 1].buttons, d);
 		}
 	}
 	for(unsigned int i = 0; i < P.size(); i++){
