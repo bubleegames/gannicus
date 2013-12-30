f929d2e2f564fb69e2fad40dca1c1ea4afcf9a17
Alex Kelly <eshink@gmail.com>
2013-12-29 21:19:57 -0800

    More informative combo counter

--- src/engine/draw.cc
+++ src/engine/draw.cc
@@ -305,10 +305,15 @@ void SaltAndBone::drawHUD()
 		}
 		if(combo[i] || knockdown[i]){
 			glColor4f(1.0, 1.0-.5*illegit[i], 1.0-.5*illegit[i], 1.0);
-			if(combo[i] > 1){ 
+			if(combo[i] > 1)
 				drawGlyph(to_string(combo[i]) + " hits", 100+800*i, 600, 400, 75, 0+2*i);
-			} else if(!knockdown[i]) glColor4f(0.5, 0.5, 0.5, 0.8);
-			drawGlyph(to_string(damage[i]) + " damage", 100+800*i, 600, 475, 35, 0+2*i);
+			if(counterHit[i] > 0)
+				glColor4f(1.0, 1.0, 0.5, 0.8);
+			else if(combo[i] < 0)
+				glColor4f(1.0, 1.0, 1.0, 0.2);
+			else if(!knockdown[i])
+				glColor4f(1.0, 1.0, 1.0, 0.8);
+			if(damage[i] != 0 || knockdown[i])drawGlyph(to_string(damage[i]) + " damage", 100+800*i, 600, 475, 35, 0+2*i);
 			if(knockdown[i]){
 				if(knockdown[i] == 1) glColor4f(0.6, 1.0, 0.6, 1.0);
 				drawGlyph("Knockdown", 100+800*i, 600, 510, 35, 0+2*i); 
